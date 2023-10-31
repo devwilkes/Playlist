ba2dca37b0cce50a61a39b2cf109ac94fe84d442
Justin Flores <jflo16@terpmail.umd.edu>
2023-10-31 11:16:42 -0400

    added filtered_songs function

--- Playlist.py
+++ Playlist.py
@@ -3,7 +3,12 @@
 class Song:
     """Represents a song in the playlist.
     """
-
+    def filtered_songs():
+        """Filters the list of songs based on user-provided criteria
+        
+        Returns:
+            A refined list of songs that match the user's criteria
+        """
 class Playlist:
     """Represents a playlist of songs.
     """
