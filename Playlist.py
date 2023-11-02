1c7b8a9b98e38c3216f149f0a1965bd02f3a9a30
Devon Wilkes <devonowilkes@hotmail.com>
2023-11-02 16:26:53 -0400

    Added __init__ methods and generate_queue
    Also added in docstrings for Song and Playlist classes.

--- Playlist.py
+++ Playlist.py
@@ -3,32 +3,51 @@
 class Song:
     """Represents a song in the playlist.
     """
-    def filtered_songs():
+
+    def __init__(self):
+        """Initializes a Song object."""
+
+    def filtered_songs(self):
         """Filters the list of songs based on user-provided criteria
-        
+
         Returns:
             A refined list of songs that match the user's criteria
         """
+
+
 class Playlist:
     """Represents a playlist of songs.
     """
-    
+
+    def __init__(self):
+        """Initializes a Playlist object."""
+
+    def generate_queue(self):
+        """ Creates a queue of songs to be played by the user. Can be generated
+        randomly or generated with user criteria.
+        """
+
+
 class Sort:
     """ A class for sorting songs in the playlist by popularity 
     """
+
     def __init__(self, songs):
         """
         initialize the sort class with a list of songs 
-        
+
         the args- songs list - a list of song dictionaries with 'name' and 
         'popularity' attributes
         """
+
     def sort_by_popularity(self, ascending=True):
         """
         sorting the songs by popularity in ascending or descending order 
-        
+
         the args- set to true for ascending , false for descending, default will be true 
         """
+
+
 class AddSong:
     """Allow user to add a song to playlist.
     """
